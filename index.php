c9cf8996077c083df051d21a13b4216b670ab3c6
Julien Desrosiers <juliend2@gmail.com>
2020-03-05 20:20:11 -0500

    z dimension is working, along with zoom, rotation and perspective

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,10 +10,28 @@
 
   <body>
     <section class="main">
-      <div id="app-container"></div>
+      <h1>APP</h1>
+
+      <div>
+        Perspective: <input type="range" value="0" step='100' min='0' max='2000' id="perspective-slider"/>
+        <span id="perspective"></span> pixels.
+      </div>
+
+      <div>
+        Rotation: <input type="range" value="0" step='10' min='0' max='90' id="angle-slider"/>
+        <span id="angle"></span> degrees.
+      </div>
+
+      <div>
+        Zoom: <input type="range" value="100" step='100' min='0' max='2000' id="zoom-slider"/>
+        <span id="zoom">100</span> %.
+      </div>
+
+      <div id="app-container" class="app-instance"></div>
     </section>
     <script src="js/app.js"></script>
     <script>
+
       const data = [
         {x: 1, y: 1, z: 1, type: 'text', content: "J'aime le chocolat"},
         {x: 1, y: 2, z: 2, type: 'text', content: "deuxieme noeud"},
@@ -31,7 +45,37 @@
         {x: 5, y: 5, z: 5, type: 'text', content: "10th"},
         {x: 6, y: 7, z: 1, type: 'text', content: "11th"},
       ];
+
       const appInstance = new App(document.querySelector('#app-container'), data);
+      const angleSpan = document.querySelector('#angle');
+      const perspectiveSpan = document.querySelector('#perspective')
+      const zoomSpan = document.querySelector('#zoom')
+
+      document.querySelector('#angle-slider').onchange = (e) => {
+        const selectedAngle = e.target.value
+        appInstance.updateRotation(selectedAngle)
+        angleSpan.innerText = selectedAngle
+      }
+
+      document.querySelector('#perspective-slider').onchange = (e) => {
+        const selectedPerspective = e.target.value
+        document.querySelector('#app-container').style.perspective = selectedPerspective + 'px'
+        perspectiveSpan.innerText = selectedPerspective
+      }
+
+      document.querySelector('#zoom-slider').onchange = (e) => {
+        const selectedZoomPct = e.target.value
+        // document.querySelector('#app-container').style.translate = 
+        appInstance.updateZoom(selectedZoomPct)
+        zoomSpan.innerText = selectedZoomPct
+      }
+
+      document.querySelectorAll('.table2D__cell').forEach((cell)=>{
+        cell.addEventListener('click', (e)=>{
+          e.target.style.outline = '1px solid red';
+        })
+      })
+
     </script>
   </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Don't care about the standard types' prototypes in this project:
 Array.prototype.max = function() {
   return Math.max.apply(null, this);
@@ -12,10 +13,17 @@ class App {
   constructor(container, data) {
     this.container = container
     this.data = data
+    this.angleDegrees = 0 // default
+    this.zoomPct = 100
     // do it:
     this.initialize()
   }
   initialize() {
+    // instantiate the data cells:
+    this.drawMatrix(this.angleDegrees)
+  }
+  drawMatrix(angleDegrees) {
+    this.container.innerHTML = ''; // empty out the container before drawing
     // get matrix width:
     const maxWidth = this.data.map(d=>d.x).max()
     // get matrix height:
@@ -26,31 +34,76 @@ class App {
     const rows = Array.from({length: maxHeight}, (v, i) => {
       return columns
     })
-    console.log('rows', rows)
+    const box = Array.from({length: maxDepth}, (v, i) => {
+      return rows
+    })
 
     // Draw it:
-    const table = new Table2d(rows, 0)
-    this.container.innerHTML = table.toString();
+    box.forEach((rows, i) => {
+      const table = new Table2d(rows, i, angleDegrees, this.zoomPct)
+      this.container.insertAdjacentHTML('beforeend', table.toString());
+    })
+
+    this.container.querySelectorAll('.table2D__cell').forEach((cell)=>{
+      cell.addEventListener('click', (e)=>{
+        e.target.style.outline = '1px solid red';
+      })
+    })
   }
+
+  updateRotation(angleDegrees) {
+    this.angleDegrees = angleDegrees
+    this.drawMatrix(angleDegrees)
+  }
+
+  updateZoom(zoomPct) {
+    this.zoomPct = zoomPct
+    this.drawMatrix(this.angleDegrees)
+  }
+}
+
+function getXTranslationPx(fromDegrees) {
+  const magicMultiplicator = 6.7
+  const maxAngleThresholdBeforeReturning = 45
+  return fromDegrees < maxAngleThresholdBeforeReturning ?
+    (fromDegrees * magicMultiplicator) :
+    ((90 - fromDegrees) * magicMultiplicator)
 }
 
 
 class Table2d {
-  constructor(rows, depth) {
+
+  constructor(rows, depth, yRotationDegrees = 0, zoomPct = 100) {
     this.rows = rows
     this.depth = depth
+    this.yRotationDegrees = yRotationDegrees
+    this.zoomPct = zoomPct
+    this.zoomFraction = this.zoomPct / 100
+    const angles = [
+      0, 10, 20, 30, 45, 50, 60, 70, 80, 90
+    ];
+    for (let i = 0; i < angles.length; i++) {
+      console.log('Table2d toString', angles[i], getXTranslationPx(angles[i]))
+    }
   }
+
   // Returns an HTML String:
   toString() {
+    const x = getXTranslationPx(this.yRotationDegrees) * (this.depth + 1)
+    const z = this.depth * 100 * this.zoomFraction
     return `
-      <table id="z${this.depth}">
+      <table id="z${this.depth}" class="table2D"
+        style="
+        transform: translate3d(${x}px, 0, ${z}px)
+          rotate3d(0, 1, 0, ${this.yRotationDegrees}deg);
+      ">
         <tbody>${
           this.rows.map((row, y_i) => {
             return `
-              <tr>
+              <tr class="table2D__row">
                 ${
                   row.map((cell, x_i) => {
-                    return `<td id="x${x_i}-y${y_i}-z${this.depth}"></td>`
+                    return `<td id="x${x_i}-y${y_i}-z${this.depth}" class="table2D__cell">&nbsp;</td>`
                   }).join('')
                 }
               </tr>
@@ -61,5 +114,6 @@ class Table2d {
       </table>
     `
   }
+
 }
 
